3fd0f45e99c3350f4049f3c5cdabe5b3cfef409b
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-25 17:12:35 +0200

    Homepage

--- view/header.php
+++ view/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Webdesign_v1</title>
+    <title>SpiceProjects</title>
     <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/fonts/ionicons.min.css">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
