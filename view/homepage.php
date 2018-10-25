e7c5be74e2c362b538ff400f603223ed4a53fd2d
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-25 23:57:20 +0200

    homepage

--- view/homepage.php
+++ view/homepage.php
@@ -39,8 +39,8 @@ use view\TemplateView;
                                 <td><?php echo TemplateView::noHTML($program->getDuration()); ?> </td>
                                 <td>
                                     <div class="btn-group btn-group-sm" role="group">
-                                        <a class="btn btn-default" role="button" href="program/edit?id=<?php echo $program->getId(); ?>"> <i class="fa fa-edit"></i></a>
-                                        <button class="btn btn-default" type="button" data-target="#confirm-modal" data-toggle="modal" data-href="program/delete?id=<?php echo $program->getId(); ?>"> <ion-icon name="trash"></ion-icon></button>
+                                        <a class="btn-default" role="button" href="program/edit?id=<?php echo $program->getId(); ?>"> <i class="fa fa-edit"></i></a>
+                                        <button class="btn-default" type="button" data-target="#confirm-modal" data-toggle="modal" data-href="program/delete?id=<?php echo $program->getId(); ?>"> <ion-icon name="trash"></ion-icon></button>
                                     </div>
                                 </td>
                             </tr>
