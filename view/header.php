db8265b4ec30421f316f3440954bb037c8eba9bd
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-26 07:17:46 +0200

    homepage

--- view/header.php
+++ view/header.php
@@ -26,6 +26,28 @@
     <meta name="msapplication-TileColor" content="#ffffff">
     <meta name="msapplication-TileImage" content="assets/img/favicon/ms-icon-144x144.png">
     <meta name="theme-color" content="#ffffff">
+    <script>
+        function searchProgram() {
+            // Declare variables
+            var input, filter, table, tr, td, i;
+            input = document.getElementById("search");
+            filter = input.value.toUpperCase();
+            table = document.getElementById("program");
+            tr = table.getElementsByTagName("tr");
+
+            // Loop through all table rows, and hide those who don't match the search query
+            for (i = 0; i < tr.length; i++) {
+                td = tr[i].getElementsByTagName("td");
+                if (td) {
+                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                        tr[i].style.display = "";
+                    } else {
+                        tr[i].style.display = "none";
+                    }
+                }
+            }
+        }
+    </script>
     <script src="https://unpkg.com/ionicons@4.4.6/dist/ionicons.js"></script>
 </head>
 
--- view/homepage.php
+++ view/homepage.php
@@ -54,26 +54,3 @@ use view\TemplateView;
         </form>
     </div>
 </div>
-
-<script>
-    function searchProgram() {
-        // Declare variables
-        var input, filter, table, tr, td, i;
-        input = document.getElementById("search");
-        filter = input.value.toUpperCase();
-        table = document.getElementById("program");
-        tr = table.getElementsByTagName("tr");
-
-        // Loop through all table rows, and hide those who don't match the search query
-        for (i = 0; i < tr.length; i++) {
-            td = tr[i].getElementsByTagName("td");
-            if (td) {
-                if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
-                    tr[i].style.display = "";
-                } else {
-                    tr[i].style.display = "none";
-                }
-            }
-        }
-    }
-</script>
