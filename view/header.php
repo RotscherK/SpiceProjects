0890319be3460fe9a07b5f670137706b9b1b0eab
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-26 07:45:41 +0200

    homepage

--- view/header.php
+++ view/header.php
@@ -37,9 +37,9 @@
 
             // Loop through all table rows, and hide those who don't match the search query
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("td");
+                rowContent = tr[i].innerHTML.toUpperCase()innerHTML.replace(/<th>/g, "").replace(/<.th>/g, "").replace(/[\n|\r|\n\r]/g,"").replace(/ /g,"");
                 if (td) {
-                    if (td.innerHTML.toUpperCase().indexOf(filter) > -1) {
+                    if (rowContent.indexOf(filter) > -1) {
                         tr[i].style.display = "";
                     } else {
                         tr[i].style.display = "none";
