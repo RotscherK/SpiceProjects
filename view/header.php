54d5c2ab2c2d41ac9f8ea22812fc5572728fe226
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-26 17:01:44 +0200

    login

--- view/header.php
+++ view/header.php
@@ -42,11 +42,11 @@
                 <li class="nav-item" role="presentation"><a class="nav-link" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/agent/edit" style="color: #ffffff;">Users</a></li>
             </ul>
             <ul class="nav navbar-nav">
-                <li class="nav-item log-button" role="presentation">
+                <li class="nav-item" role="presentation">
                     <?php if(isset($_SESSION['userLogin'])): ?>
-                        <a class="btn nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/login" >Logout <ion-icon name="log-out"></ion-icon></a>
+                        <a class="log-button btn nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/login" >Logout <ion-icon name="log-out"></ion-icon></a>
                     <?php else: ?>
-                        <a class="btn nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/login" >Login <ion-icon name="log-in"></ion-icon></span></a>
+                        <a class="log-button btn nav-link active" href="<?php echo $GLOBALS["ROOT_URL"]; ?>/login" >Login <ion-icon name="log-in"></ion-icon></span></a>
                     <?php endif; ?>
                 </li>
             </ul>
