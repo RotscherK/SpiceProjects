9f9456d2936bf0f3b902e96633b1d293399797a3
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-26 09:55:01 +0200

    homepage

--- view/header.php
+++ view/header.php
@@ -29,17 +29,18 @@
     <script>
         function searchProgram() {
             // Declare variables
-            var input, filter, table, tr, td, i, rowContent;
+            var input, filter, table, tbody, tr, td, i, rowContent;
             input = document.getElementById("search");
             filter = input.value.toUpperCase();
             table = document.getElementById("program");
-            tr = table.getElementsByTagName("tr");
+            tbody = document.getElementsByTagName("tbody")[0];
+            tr = tbody.getElementsByTagName("tr");
 
             // Loop through all table rows, and hide those who don't match the search query
             for (i = 0; i < tr.length; i++) {
-                td = tr[i].getElementsByTagName("th");
-                alert(td);
+                td = tr[i].getElementsByTagName("td");
                 rowContent = td[0].innerHTML + td[1].innerHTML + td[2].innerHTML + td[3].innerHTML + td[4].innerHTML + td[5].innerHTML;
+
                 if (td) {
                     if (rowContent.toUpperCase().indexOf(filter) > -1) {
                         tr[i].style.display = "";
