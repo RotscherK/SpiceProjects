ba5da08211cac6f9f6b7eb2ac9ec34ae679dd04f
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-29 14:57:12 +0100

    Program Edit

--- view/programEdit.php
+++ view/programEdit.php
@@ -15,19 +15,64 @@ isset($this->programValidator) ? $programValidator = $this->programValidator : $
 
 <div class="col-sm-10 text-left blue-background">
     <div class="form-clean">
-
         <form action="update" method="post">
+            <h2 class="text-center">Create/Edit program</h2>
             <div class="form-group row">
-                <label for="inputEmail3" class="col-sm-2 col-form-label">Email</label>
+                <label for="id" class="col-sm-2 col-form-label">ID</label>
                 <div class="col-sm-10">
-                    <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+                    <input type="text" class="form-control" id="id" placeholder="ID" readonly="true" <?php echo $program->getId() ?>">
                 </div>
             </div>
             <div class="form-group row">
-                <label for="inputPassword3" class="col-sm-2 col-form-label">Password</label>
+                <label for="type" class="col-sm-2 col-form-label">Type</label>
                 <div class="col-sm-10">
-                    <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+                    <select class="form-control" id="type">
+                        <option value="1" <?php if($program->getType() == '1'): ?> selected="selected"<?php endif; ?> >BB</option>
+                        <option value="2" <?php if($program->getType() == '2'): ?> selected="selected"<?php endif; ?> >VZ</option>
+                        <option value="3" <?php if($program->getType() == '3'): ?> selected="selected"<?php endif; ?> >VZ/BB</option>
+                    </select>
                 </div>
+            </div>
+            <div class="form-group row">
+                <label for="category" class="col-sm-2 col-form-label">Type</label>
+                <div class="col-sm-10">
+                    <select class="form-control" id="category">
+                        <option value="1">Computer Science</option>
+                        <option value="2">Economy</option>
+                        <option value="3">Education</option>
+                        <option value="4">Social</option>
+                        <option value="5">Architect</option>
+                    </select>
+                </div>
+            </div>
+
+            <div class="form-group row <?php echo $programValidator->isNameError() ? "has-error" : ""; ?>">
+                <label for="name" class="col-sm-2 col-form-label">Name</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="name" placeholder="Name">
+                </div>
+                <p class="help-block"><?php echo $programValidator->getNameError() ?></p>
+            </div>
+            <div class="form-group row <?php echo $programValidator->isDescriptionError() ? "has-error" : ""; ?>">
+                <label for="description" class="col-sm-2 col-form-label">Description</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="description" placeholder="Description">
+                </div>
+                <p class="help-block"><?php echo $programValidator->getDescriptionError() ?></p>
+            </div>
+            <div class="form-group row <?php echo $programValidator->isPriceError() ? "has-error" : ""; ?>">
+                <label for="price" class="col-sm-2 col-form-label">Price</label>
+                <div class="col-sm-10">
+                    <input type="number" class="form-control" id="price" placeholder="Price">
+                </div>
+                <p class="help-block"><?php echo $programValidator->getPriceError() ?></p>
+            </div>
+            <div class="form-group row <?php echo $programValidator->isPriceError() ? "has-error" : ""; ?>">
+                <label for="price" class="col-sm-2 col-form-label">Price</label>
+                <div class="col-sm-10">
+                    <input type="text" class="form-control" id="price" placeholder="Price">
+                </div>
+                <p class="help-block"><?php echo $programValidator->getPriceError() ?></p>
             </div>
             <fieldset class="form-group">
                 <div class="row">
