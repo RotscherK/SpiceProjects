ab0a907287ffb415c9ea62e06f14c5622ef0024d
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-10-31 14:02:36 +0100

    Reset

--- view/userPasswordReset.php
+++ view/userPasswordReset.php
@@ -1,9 +1,9 @@
 <?php
 /**
  * Created by PhpStorm.
- * User: andreas.martin
- * Date: 13.09.2017
- * Time: 21:28
+ * User: roger.kaufmann
+ * Date: 31.10.2018
+ * Time: 14:01
  */
 
 use view\TemplateView;
@@ -12,9 +12,9 @@ use view\TemplateView;
 <div class="col-sm-10 text-left blue-background">
     <div class="form-clean">
         <div class="page-header">
-            <h2 class="text-center"><strong>I forgot my password.</strong></h2></div>
         <form action="<?php echo $GLOBALS["ROOT_URL"]; ?>/password/reset" method="post">
-            <input type="hidden" name="token" value="<?php echo TemplateView::noHTML($this->token); ?>"/>
+            <h2 class="text-center"><strong>Reset your Password</strong></h2></div>
+        <input type="hidden" name="token" value="<?php echo TemplateView::noHTML($this->token); ?>"/>
             <div class="form-group <?php echo isset($this->agentValidator) && $this->agentValidator->isPasswordError() ? "has-error" : ""; ?>">
                 <input class="form-control" type="password" name="password" placeholder="Password">
                 <p class="help-block"><?php echo isset($this->agentValidator) && $this->agentValidator->isPasswordError() ? $this->agentValidator->getPasswordError() : ""; ?></p>
