6cd48ddc8a205b0ab2e2b99b5733a4f1d7e9ad98
timothyapplewhite <timothy.applewhite@students.fhnw.ch>
2018-11-01 19:27:30 +0100

    Tim

--- view/userList.php
+++ view/userList.php
@@ -7,39 +7,45 @@
  */
 
 use view\TemplateView;
-use domain\User;
-isset($this->user) ? $user = $this->user : $user = new User();
+use domain\Program;
+isset($this->program) ? $program = $this->program : $program = new Program();
 
 ?>
 <div class="col-sm-10 text-left blue-background">
     <div class="form-clean">
         <form method="post">
-            <h2 class="text-center">Available Users</h2>
-            <div class="form-group"><input id="search" class="form-control" type="search" onkeyup="searchUser()" placeholder="Search"></div>
+            <h2 class="text-center">Available Programs</h2>
+            <div class="form-group"><input id="search" class="form-control" type="search" onkeyup="searchProgram()" placeholder="Search"></div>
             <div class="text">
                 <div class="table-responsive table table-striped table-bordered table-hover">
-                    <table class="table" id="user">
+                    <table class="table" id="program">
                         <thead>
                         <tr>
-                            <th>Last Name1</th>
-                            <th>First Name</th>
-                            <th>Email</th>
+                            <th>Program</th>
+                            <th>University</th>
+                            <th>Place</th>
+                            <th>Degree</th>
+                            <th>Type</th>
+                            <th>Duration</th>
                             <?php if(isset($_SESSION['userLogin'])): ?><th>Action</th><?php endif; ?>
 
                         </tr>
                         </thead>
                         <tbody>
                         <?php
-                        foreach($this->users as $user): /* @var User $user */ ?>
-                            <tr class='clickable-row' data-href="/user?id=<?php echo $user->getId(); ?>">
-                                <td><?php echo TemplateView::noHTML($user->getLastname()); ?></td>
-                                <td><?php echo TemplateView::noHTML($user->getFirstname()); ?></td>
-                                <td><?php echo TemplateView::noHTML($user->getEmail()); ?></td>
+                        foreach($this->programs as $program): /* @var Program $program */ ?>
+                            <tr class='clickable-row' data-href="/program?id=<?php echo $program->getId(); ?>">
+                                <td><?php echo TemplateView::noHTML($program->getName()); ?></td>
+                                <td><?php echo TemplateView::noHTML($program->getProviderId()); ?></td>
+                                <td><?php echo TemplateView::noHTML($program->getPrice()); ?> ! <?php echo TemplateView::noHTML($program->getCategoryId()); ?> ! <?php echo TemplateView::noHTML($program->getDistanceLearning()); ?> ! <?php echo TemplateView::noHTML($program->getURL()); ?></td>
+                                <td><?php echo TemplateView::noHTML($program->getDegree()); ?> </td>
+                                <td><?php echo TemplateView::noHTML($program->getType()); ?> </td>
+                                <td><?php echo TemplateView::noHTML($program->getDuration()); ?> </td>
                                 <?php if(isset($_SESSION['userLogin'])): ?>
                                     <td>
                                         <div class="btn-group btn-group-sm" role="group">
-                                            <a class="btn-default" role="button" href="user/edit?id=<?php echo $user->getId(); ?>"> <ion-icon name="create"></ion-icon></a>
-                                            <a class="btn-default" role="button" href="user/delete?id=<?php echo $user->getId(); ?>"> <ion-icon name="trash"></ion-icon></a>
+                                            <a class="btn-default" role="button" href="program/edit?id=<?php echo $program->getId(); ?>"> <ion-icon name="create"></ion-icon></a>
+                                            <a class="btn-default" role="button" href="program/delete?id=<?php echo $program->getId(); ?>"> <ion-icon name="trash"></ion-icon></a>
                                         </div>
                                     </td>
                                 <?php endif; ?>
@@ -55,18 +61,20 @@ isset($this->user) ? $user = $this->user : $user = new User();
     </div>
 </div>
 <script>
-    function searchUser() {
+    function searchProgram() {
         // Declare variables
         var input, filter, table, tbody, tr, td, i, rowContent;
         input = document.getElementById("search");
         filter = input.value.toUpperCase();
-        table = document.getElementById("user");
+        table = document.getElementById("program");
         tbody = document.getElementsByTagName("tbody")[0];
         tr = tbody.getElementsByTagName("tr");
+
         // Loop through all table rows, and hide those who don't match the search query
         for (i = 0; i < tr.length; i++) {
             td = tr[i].getElementsByTagName("td");
             rowContent = td[0].innerHTML + td[1].innerHTML + td[2].innerHTML + td[3].innerHTML + td[4].innerHTML + td[5].innerHTML;
+
             if (td) {
                 if (rowContent.toUpperCase().indexOf(filter) > -1) {
                     tr[i].style.display = "";
@@ -81,4 +89,5 @@ isset($this->user) ? $user = $this->user : $user = new User();
             window.location = $(this).data("href");
         });
     };
+
 </script>
