865b9c42762348a6dae0859ce4e78efd3570ada7
timothyapplewhite <timothy.applewhite@students.fhnw.ch>
2018-11-07 12:34:29 +0100

    Tim

--- dao/UserDAO.php
+++ dao/UserDAO.php
@@ -87,7 +87,7 @@ class UserDAO extends BasicDAO {
         $stmt->bindValue(':firstname', $user->getFirstname());
         $stmt->bindValue(':email', $user->getEmail());
         $stmt->bindValue(':password', $user->getPassword());
-        $stmt->bindValue(':siteAdmin', $user->getSiteAdmin());
+        //$stmt->bindValue(':siteAdmin', $user->getSiteAdmin());
         $stmt->bindValue(':providerAdmin', $user->getProviderAdmin());
         $stmt->bindValue(':adAdmin', $user->getAdAdmin());
         $stmt->execute();
