b62dbf72628c670daa033c2d3374c7fa08af85d9
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-11-19 15:06:30 +0100

    404

--- view/404page.php
+++ view/404page.php
@@ -8,12 +8,15 @@ isset($this->exception) ? $exception = $this->exception : $exception = new \http
 ?>
 
 <div class="col-sm-10 text-left blue-background">
-    <div class="container" style="display:flex;flex-direction:column;justify-content:center;">
-        <div class="page-header">
-            <h2 class="text-center"><strong>SpiceProject</strong></h2></div>
-        <section>
-            <h3 class="text-center">404<strong> Not Found</strong> </h3>
-            <p class="text-center">We couldn't find this url on our server. Maybe you'd like to try our <a href="<?php echo $GLOBALS["ROOT_URL"]; ?>">homepage</a> instead? </p>
-        </section>
+    <div class="form-clean">
+        <form method="post">
+            <h2 class="text-center">Spice Project</h2>
+            <div class="text">
+                <h3 class="text-center">404<strong> Not Found</strong> </h3>
+                <p class="text-center">We couldn't find this url on our server. Maybe you'd like to try our <a href="<?php echo $GLOBALS["ROOT_URL"]; ?>">homepage</a> instead? </p>
+
+
+            </div>
+        </form>
     </div>
 </div>
