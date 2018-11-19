7f99f73f267b550b1c118b1515f99bddb7ed18d5
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-11-19 14:52:22 +0100

    404

--- index.php
+++ index.php
@@ -131,7 +131,7 @@ Router::route_auth("GET", "/program/edit", $authFunction, function () {
 
 Router::route_auth("GET", "/program/delete", $authFunction, function () {
     ProgramController::delete();
-    Router::redirect("/");
+    ProgramController::readAll();
 });
 
 Router::route_auth("POST", "/program/update", $authFunction, function () {
