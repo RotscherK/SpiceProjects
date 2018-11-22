ffaf9547d24e001a65cbff329efcc0098c1d298f
Tinkels <nicola.niklaus@students.fhnw.ch>
2018-11-22 11:04:25 +0100

    delete advertisement

--- dao/AdvertisementDAO.php
+++ dao/AdvertisementDAO.php
@@ -37,10 +37,10 @@ class AdvertisementDAO extends BasicDAO {
     public function delete(Advertisement $advertisement) {
         $stmt = $this->pdoInstance->prepare('
             DELETE FROM "advertisement"
-            WHERE id = :id;
-        ');
+            WHERE id = :id;');
         $stmt->bindValue(':id', $advertisement->getId());
         $stmt->execute();
+        return $this->read($advertisement->getId());
     }
 
     /**
