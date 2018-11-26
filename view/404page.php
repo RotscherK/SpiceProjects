b8644065e808a537d00994cde8ecf50809a9e621
timothyapplewhite <timothy.applewhite@students.fhnw.ch>
2018-11-26 14:41:58 +0100

    Tim

--- view/404page.php
+++ view/404page.php
@@ -14,7 +14,7 @@ isset($this->exceptionText) ? $exceptionText = $this->exceptionText : $exception
             <h2 class="text-center">Spice Project</h2>
             <div class="text">
                 <h3 class="text-center"><?php echo $exceptionCode; ?><strong> <?php echo $exceptionText;?> </strong></h3>
-                <p class="text-center">To get back to the <a href="<?php echo $GLOBALS["ROOT_URL"]; ?>">homepage</a></p>
+                <p class="text-center">Get back to the <a href="<?php echo $GLOBALS["ROOT_URL"]; ?>">homepage</a></p>
             </div>
         </form>
     </div>
