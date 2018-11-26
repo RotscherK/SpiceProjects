9dd17b8f6f3955cbd25e4d9db6e89e3ef96bfb81
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-11-26 21:53:57 +0100

    Program

--- service/PDFServiceClient.php
+++ service/PDFServiceClient.php
@@ -22,35 +22,19 @@ class PDFServiceClient
         $options = ["http" => [
             "method" => "POST",
             "header" => ["Content-Type: application/json"],
-            "content" => json_encode($jsonObj),
-            'ignore_errors' => true
+            "content" => json_encode($jsonObj)
         ]];
         $context = stream_context_create($options);
 
+        print_r($options);
+
         $response = file_get_contents("https://www.hypdf.com/htmltopdf", false, $context);
-        print_r(self::parseHeaders($http_response_header));
+        //print_r($http_response_header);
         echo "<br><br><br>";
+
         if(strpos($http_response_header[0],"200"))
             return $response;
         return false;
-    }
-
-    static function  parseHeaders( $headers )
-    {
-        $head = array();
-        foreach( $headers as $k=>$v )
-        {
-            $t = explode( ':', $v, 2 );
-            if( isset( $t[1] ) )
-                $head[ trim($t[0]) ] = trim( $t[1] );
-            else
-            {
-                $head[] = $v;
-                if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
-                    $head['reponse_code'] = intval($out[1]);
-            }
-        }
-        return $head;
     }
 
     protected static function createPDFJSONObj(){
