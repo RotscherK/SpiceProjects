b352998d93da5124adec13ebe105e105f12a93d3
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-11-26 18:57:08 +0100

    Program

--- view/programInvoicePDF.php
+++ view/programInvoicePDF.php
@@ -4,35 +4,35 @@ use domain\Program;
 
 ?>
 
-<h1 align="center" style="font-family: Helvetica;"><span style="color: #f4476b;">Spice Projects</span> | Program Details</h1>
+<h1 align="center" style="font-family: Helvetica"><span style="color: #f4476b;">Spice Projects</span> | Monthly invoice</h1>
 <br>
-<table border="0" style="height: 237px; border-color: #f4476b; border-collapse: collapse; width: 463.5px; margin-left: auto; margin-right: auto; font-size: 20px;" >
+<table border="0" style="height: 237px; border-color: #f4476b; border-collapse: collapse; width: 463.5px; margin-left: auto; margin-right: auto; font-family: Helvetica; font-size: 15px;" >
     <thead>
     <tr>
-        <td style="width: 186px; padding-left: 10px; font-family: Helvetica; font-weight: bold;">Name</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">Degree</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">Duration</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">Expiration Date</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">Cost</td>
+        <td style=" padding-left: 10px;font-weight: bold;">Name</td>
+        <td style=" padding-left: 10px;font-weight: bold;">Degree</td>
+        <td style=" padding-left: 10px;font-weight: bold;">Duration</td>
+        <td style=" padding-left: 10px;font-weight: bold;">Expiration Date</td>
+        <td style=" padding-left: 10px;font-weight: bold;">Cost</td>
     </tr>
     </thead>
     <tbody>
     <?php foreach($this->billingPrograms as $program): /* @var Program $program */ ?>
         <tr>
-            <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($program->getName()); ?></td>
-            <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($program->getDegree()); ?> </td>
-            <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($program->getDuration()); ?> </td>
-            <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo TemplateView::noHTML($program->getStartDate()); ?> </td>
-            <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">1.- CHF</td>
+            <td style="padding-left: 10px;"><?php echo TemplateView::noHTML($program->getName()); ?></td>
+            <td style=" padding-left: 10px;"><?php echo TemplateView::noHTML($program->getDegree()); ?> </td>
+            <td style=" padding-left: 10px;"><?php echo TemplateView::noHTML($program->getDuration()); ?> </td>
+            <td style=" padding-left: 10px;"><?php echo TemplateView::noHTML($program->getStartDate()); ?> </td>
+            <td style=" padding-left: 10px;">1.- CHF</td>
         </tr>
     <?php endforeach; ?>
     <tr class='clickable-row' data-href="/program?id=<?php echo $program->getId(); ?>">
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"></td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"></td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"></td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"></td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;">Total</td>
-        <td style="width: 274.5px; padding-left: 10px; font-family: Helvetica;"><?php echo sizeof($this->billingPrograms); ?> CHF</td>
+        <td style=" padding-left: 10px;"></td>
+        <td style=" padding-left: 10px;"></td>
+        <td style=" padding-left: 10px;"></td>
+        <td style=" padding-left: 10px;"></td>
+        <td style=" padding-left: 10px; font-weight: bold;">Total</td>
+        <td style=" padding-left: 10px; font-weight: bold;"><?php echo sizeof($this->billingPrograms); ?> CHF</td>
 
     </tr>
     </tbody>
