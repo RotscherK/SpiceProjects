620cb9f7fcdbeb8512ec345cc6494c60c5d7192f
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-11-26 21:44:01 +0100

    Program

--- service/PDFServiceClient.php
+++ service/PDFServiceClient.php
@@ -28,7 +28,8 @@ class PDFServiceClient
         $context = stream_context_create($options);
 
         $response = file_get_contents("https://www.hypdf.com/htmltopdf", false, $context);
-        print_r($http_response_header);
+        print_r(parseHeaders($http_response_header));
+        echo "<br><br><br>";
         if(strpos($http_response_header[0],"200"))
             return $response;
         return false;
