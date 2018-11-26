54b40542c8f711381cf90c859b73c4148137fff9
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-11-26 21:45:24 +0100

    Program

--- service/PDFServiceClient.php
+++ service/PDFServiceClient.php
@@ -35,6 +35,24 @@ class PDFServiceClient
         return false;
     }
 
+    function parseHeaders( $headers )
+    {
+        $head = array();
+        foreach( $headers as $k=>$v )
+        {
+            $t = explode( ':', $v, 2 );
+            if( isset( $t[1] ) )
+                $head[ trim($t[0]) ] = trim( $t[1] );
+            else
+            {
+                $head[] = $v;
+                if( preg_match( "#HTTP/[0-9\.]+\s+([0-9]+)#",$v, $out ) )
+                    $head['reponse_code'] = intval($out[1]);
+            }
+        }
+        return $head;
+    }
+
     protected static function createPDFJSONObj(){
         return json_decode('{"content": "HTML", "user": "HYPDF_USER", "password": "YOUR_HYPDF_PASSWORD", "test": "true"}');
     }
