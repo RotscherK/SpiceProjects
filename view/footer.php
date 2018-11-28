7131e7a153d0ad1d601a75ec9164641f9af4779e
nicola <nicola.niklaus@students.fhnw.ch>
2018-11-28 16:17:46 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/dataSources/e5d17e67-8e46-498c-b278-28662854ab4f.xml

--- view/footer.php
+++ view/footer.php
@@ -29,7 +29,7 @@ $advertisements = (new AdvertisementServiceImpl())->getAllAdvertisements();
         <div class="container">
             <div class="row">
                 <div class="col-sm-6 col-md-3 item">
-                    <h3>Services</h3>
+                    <h3>Services1</h3>
                     <ul>
                         <li><a href="#">Web design</a></li>
                         <li><a href="#">Development</a></li>
