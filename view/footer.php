cde05be78e66d23f6e5ea29c8476848f46307621
Tinkels <nicola.niklaus@students.fhnw.ch>
2018-11-30 13:05:02 +0100

    create advertisement --> index entry

--- view/footer.php
+++ view/footer.php
@@ -13,7 +13,7 @@ $advertisements = (new AdvertisementServiceImpl())->getAllAdvertisements();
         shuffle($advertisements);
         foreach($advertisements as $advertisement): /* @var Advertisement $advertisement */ ?>
         <?php $i = 0; ?>
-            <?php if (++$i > 9)break; ?>
+            <?php if (++$i > 8)break; ?>
         <?php $link = $advertisement->getURL();?>
         <?php echo "<p><a href='$link'>";?>
             <?php echo TemplateView::noHTML($advertisement->getTitle()); ?>
