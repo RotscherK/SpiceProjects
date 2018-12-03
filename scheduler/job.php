834e557412c73c550fea7ba4abce59ed586e5c1d
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-12-03 21:52:27 +0100

    SQL

--- scheduler/job.php
+++ scheduler/job.php
@@ -8,9 +8,9 @@ use controller\ProgramController;
 //Execution of invoicing at the end of the month
 $d = new DateTime();
 
-//if($d->format( 'Y-m-t' ) == $d->format( 'Y-m-d' )){
+if($d->format( 'Y-m-t' ) == $d->format( 'Y-m-d' )){
     ChargingController::charging();
-//}
+}
 
 //Daily check of expired programs
 ProgramController::expirationNotification();
