3a3d0c6d0a9d660eaf644ba6ef1229a3d08fbe0f
Rotscher <roger.kaufmann1@students.fhnw.ch>
2018-12-03 20:58:56 +0100

    Program

--- service/EmailServiceClient.php
+++ service/EmailServiceClient.php
@@ -36,6 +36,8 @@ class EmailServiceClient
         $context = stream_context_create($options);
 
         $response = file_get_contents("https://api.sendgrid.com/v3/mail/send", false, $context);
+        $response = file_get_contents("https://api.sendgrid.com/v3/mail/send", false, $context);
+
         if(strpos($http_response_header[0],"202"))
             return true;
         return false;
