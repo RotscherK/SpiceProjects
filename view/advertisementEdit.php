e9cbf66ef18150296ad140aafdb5e82635a2f5b8
Tinkels <nicola.niklaus@students.fhnw.ch>
2018-12-11 15:51:48 +0100

    create advertisement --> index entry

--- view/advertisementEdit.php
+++ view/advertisementEdit.php
@@ -74,7 +74,7 @@ isset($this->user) ? $user = $this->user : $user = new User();
                     'region' => 'eu-west-2'
                 ]);
                 if(isset($_FILES['file'])) {
-                    var_dump($file);
+                    var_dump($_FILES);
                     $file = $_FILES['file'];
                     //File details
                     $name =$file['name'];
