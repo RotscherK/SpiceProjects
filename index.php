45cb03e9840c0f75fae710e28a4a8ae107293a01
timothyapplewhite <timothy.applewhite@students.fhnw.ch>
2018-12-19 16:35:51 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	view/programDetail.php

--- index.php
+++ index.php
@@ -169,8 +169,12 @@ Router::route("GET", "/program/pdf/{id}", function ($id) {
 });
 
 Router::route("POST", "/program/request", function(){
-    if(EmailController::sendRequestInformation());
-    Router::redirect("/");
+    if(EmailController::sendRequestInformation()){
+        Router::redirect("/");
+    }else{
+        Router::redirect("/1");
+    }
+
 });
 
 
