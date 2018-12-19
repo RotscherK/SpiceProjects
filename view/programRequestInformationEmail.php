4d981e43c51e42b516097c52101ca996daadd3b0
timothyapplewhite <timothy.applewhite@students.fhnw.ch>
2018-12-19 17:42:31 +0100

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	view/programDetail.php

--- view/programRequestInformationEmail.php
+++ view/programRequestInformationEmail.php
@@ -10,8 +10,12 @@ isset($this->program) ? $program = $this->program : $program = new Program();
 <html>
 <body>
 <p>Dear Provider,</p>
-<p>A question has been asked on one of your programs:</p>
+<p>A question has been asked on one of your programs: <?php echo $program->getName(); ?>. </p>
 
+<p>Name: <?php echo $this->name; ?>. </p>
+<p>Email: <?php echo $this->email; ?>. </p>
+<p>Phone: <?php echo $this->phone; ?>. </p>
+<p>Comment: <?php echo $this->comment; ?>. </p>
 <p>Kind regards</p>
 <p>Your Spice Projects Team</p>
 </body>
